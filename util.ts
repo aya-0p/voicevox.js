254d12f948662eac5402206239cb9ee08aed4d30
aya <4587game.aya@gmail.com>
2024-01-29 14:54:26 +0900

    devバージョン削除

--- util.ts
+++ util.ts
@@ -14,40 +14,6 @@ export class VoicevoxError extends Error {}
  */
 export const Core = Symbol("Core");
 
-/**
- * Buffer形式のuuidを文字列に変換
- * @example
- * ```js
- * bufferToUuid(uuid); // "01234567-89ab-cdef-0123-456789abcdef"
- * ```
- */
-export function bufferToUuid(buf: Buffer) {
-  if (buf.length !== 16) throw new VoicevoxJsError("有効なBufferUuidではありません。");
-  let str = "";
-  for (let i = 0; i < buf.length; i++) {
-    str += buf[i].toString(16).padStart(2, "0");
-    if (i === 4 || i === 6 || i === 8 || i === 10) str += "-";
-  }
-  return str;
-}
-
-/**
- * 文字列のuuidをBuffer形式に変換
- * @example
- * ```js
- * uuidToBuffer(uuid); // <Buffer 01 23 45 67 89 ab cd ef 01 23 45 67 89 ab cd ef>
- * ```
- */
-export function uuidToBuffer(uuid: string) {
-  const t = Buffer.from(uuid.replaceAll("-", ""), "hex");
-  if (t.length !== 16) throw new VoicevoxJsError("uuidが不正な値です");
-  const buf = Buffer.alloc(16).fill(0);
-  for (let i = 15; i >= 0; i--) {
-    buf[i] = t[i];
-  }
-  return buf;
-}
-
 export function checkValidOption<T extends { [key: string]: any }>(obj: T, interfaceName: string, options: Array<[keyof T, "boolean" | "number" | "string", true?]>) {
   if (obj == null) throw new VoicevoxJsError(`有効な${interfaceName}ではありません(存在しない)`);
   for (const [name, type, isInteger] of options) {
